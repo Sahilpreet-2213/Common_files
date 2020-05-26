9ecc8cd3f0a99076e1d28b573f83d8a166f7ea60
Sahilpreet_2213 <sahilpreets2003@gmail.com>
2020-05-26 18:31:02 +0530

    deleted

--- Python-Site/Django/prac/prac_/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import * 
-
-admin.site.register(Images)
-admin.site.register(Contact)
-# admin.site.register(To_image)
--- Python-Site/Django/prac/prac_/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PracConfig(AppConfig):
-    name = 'prac_'
